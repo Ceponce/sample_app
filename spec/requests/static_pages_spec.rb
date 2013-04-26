417e7970d8e35f2dc508c83f49dc1e09e37dfb4c
Juan Carlos Ponce <cpfiro@gmail.com>
2013-04-26 02:06:28 -0700

    improved root testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,19 +6,18 @@ let(:str) { "Ruby on Rails Tutorial Sample App" }
 
 #------------------------
  describe "Home page" do
+  before { visit root_path}
+
   it "should have the h1 'Sample App'" do 
-  visit root_path
   page.should have_selector( 'h1', :text => 'Sample App')
  end
 
  it "should have the base title" do
-   visit root_path
    page.should have_selector('title',
 			:text => "#{str}")
  end
 
  it "should not have a custom page title" do
-   visit root_path
    page.should_not have_selector('title', :text => "| Home")
  end
 end
