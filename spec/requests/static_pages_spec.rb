b1d94abe71339f6891e18e02754797e4759ae42a
Juan Carlos Ponce <cpfiro@gmail.com>
2013-04-26 01:51:16 -0700

    help about contact... better paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,18 +7,18 @@ let(:str) { "Ruby on Rails Tutorial Sample App" }
 #------------------------
  describe "Home page" do
   it "should have the h1 'Sample App'" do 
-  visit '/static_pages/home'
+  visit root_path
   page.should have_selector( 'h1', :text => 'Sample App')
  end
 
  it "should have the base title" do
-   visit '/static_pages/home'
+   visit root_path
    page.should have_selector('title',
 			:text => "#{str}")
  end
 
  it "should not have a custom page title" do
-   visit '/static_pages/home'
+   visit root_path
    page.should_not have_selector('title', :text => "| Home")
  end
 end
@@ -26,12 +26,12 @@ end
 #------------------------
  describe "Help page" do
   it "should have the h1 'Help'" do 
-  visit '/static_pages/help'
+  visit help_path
   page.should have_selector( 'h1', :text => 'Help')
  end
 
  it "should have the right title 'Help'" do
-   visit '/static_pages/help'
+   visit help_path
    page.should have_selector("title", :text => "#{str} | Help")
  end
 end
@@ -39,12 +39,12 @@ end
 #---------------------
  describe "About page" do
   it "should have the h1 'About Us'" do 
-  visit '/static_pages/about'
+  visit about_path
   page.should have_selector( 'h1', :text => 'About Us')
  end
 
  it "should have the right title 'About Us'" do
-   visit '/static_pages/about'
+   visit about_path
    page.should have_selector('title',
 			:text => "#{str} | About Us")
   end
@@ -53,12 +53,12 @@ end
 #--------------------
  describe "Contact page" do
   it "should have the h1 'Contact'" do
-  visit '/static_pages/contact'
+  visit contact_path
   page.should have_selector( 'h1', :text => 'Contact')
  end
 
  it "should have the right title 'Contact'" do 
-  visit '/static_pages/contact'
+  visit contact_path
   page.should have_selector( 'title', :text => "#{str} | Contact")
   end
  end 
