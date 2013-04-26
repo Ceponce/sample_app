7c46bd8c543c3344181612358d54e6c2d3186fab
Juan Carlos Ponce <cpfiro@gmail.com>
2013-04-26 02:28:54 -0700

    static_pages_spec reformated

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,47 +11,28 @@ subject { page }
   before { visit root_path}
 
   it {should have_selector( 'h1', text: 'Sample App')}
- 
- it { should have_selector('title', text: full_title(''))}
+  it { should have_selector('title', text: full_title(''))}
 end
 
 #------------------------
  describe "Help page" do
-  it "should have the h1 'Help'" do 
-  visit help_path
-  page.should have_selector( 'h1', :text => 'Help')
- end
+    before { visit help_path}
 
- it "should have the right title 'Help'" do
-   visit help_path
-   page.should have_selector("title", :text => "#{str} | Help")
- end
+  it {should have_selector( 'h1', text: 'Help')}
+  it {should have_selector("title", text: full_title('Help')) }
 end
 
 #---------------------
  describe "About page" do
-  it "should have the h1 'About Us'" do 
-  visit about_path
-  page.should have_selector( 'h1', :text => 'About Us')
- end
-
- it "should have the right title 'About Us'" do
-   visit about_path
-   page.should have_selector('title',
-			:text => "#{str} | About Us")
-  end
+    before { visit about_path}
+  it {should have_selector( 'h1', text: 'About Us')}
+  it {should have_selector('title', text: full_title('About'))}
  end
 
 #--------------------
  describe "Contact page" do
-  it "should have the h1 'Contact'" do
-  visit contact_path
-  page.should have_selector( 'h1', :text => 'Contact')
- end
-
- it "should have the right title 'Contact'" do 
-  visit contact_path
-  page.should have_selector( 'title', :text => "#{str} | Contact")
-  end
+    before { visit contact_path}
+  it {should have_selector( 'h1', text: 'Contact')}
+  it { should have_selector( 'title', text: full_title('Contact'))}
  end 
 end
