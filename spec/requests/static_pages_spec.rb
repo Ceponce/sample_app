e05754f29e1cbb76844c33045137bff2f89896da
Juan Carlos Ponce <cpfiro@gmail.com>
2013-04-26 02:20:44 -0700

    solid root test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,21 +5,14 @@ describe "Static Pages" do
 let(:str) { "Ruby on Rails Tutorial Sample App" }
 
 #------------------------
+subject { page }
+
  describe "Home page" do
   before { visit root_path}
 
-  it "should have the h1 'Sample App'" do 
-  page.should have_selector( 'h1', :text => 'Sample App')
- end
-
- it "should have the base title" do
-   page.should have_selector('title',
-			:text => "#{str}")
- end
-
- it "should not have a custom page title" do
-   page.should_not have_selector('title', :text => "| Home")
- end
+  it {should have_selector( 'h1', text: 'Sample App')}
+ 
+ it { should have_selector('title', text: full_title(''))}
 end
 
 #------------------------
