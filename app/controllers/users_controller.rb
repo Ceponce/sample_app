99163d91065a8ddff7b1129d10f430240d4ab2a4
Carlos Ponce <cpfiro@gmail.com>
2013-06-30 02:22:07 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to Clean Slate!"
       redirect_to @user
     else      
@@ -25,5 +26,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 end
